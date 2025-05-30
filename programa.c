898deee23d33f63bdf798d216799dfead58b186b
FredPoerschke <fredericolinckpoerschke@gmail.com>
2025-05-30 00:08:04 -0300

    Commit 3005

--- programa.c
+++ programa.c
@@ -7,24 +7,24 @@
 #include <string.h>
 #include <stdarg.h>
 
-#define MAX_BUFFER_SIZE 10000  // Tamanho máximo do buffer de saída
+#define MAX_BUFFER_SIZE 10000
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex_output = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond_caminhao = PTHREAD_COND_INITIALIZER;
 pthread_cond_t cond_funcionario = PTHREAD_COND_INITIALIZER;
 
-int n_caix_areacarga = 0;  // Caixas na área de carga
-int n_caix_entregues = 0;  // Caixas entregues pelos caminhões
-int n_caix_estoque = 0;    // Caixas movidas para o estoque
+int n_caix_areacarga = 0;
+int n_caix_entregues = 0;
+int n_caix_estoque = 0;
 int N, B, C, F, T, L;
 bool finalizado = false;
+bool criar_caminhoes = true; // Controla a criação de novos caminhões
 
-char output_buffer[MAX_BUFFER_SIZE];  // Buffer de saída para armazenar logs
+char output_buffer[MAX_BUFFER_SIZE];
 
 const char *funcionarios[] = {"Bilguêiti", "Torvalino", "Ólanmusgo"};
 
-// Função protegida por mutex que adiciona texto formatado ao buffer de saída
 void log_buffer(const char *fmt, ...) {
     pthread_mutex_lock(&mutex_output);
     va_list args;
@@ -36,59 +36,57 @@ void log_buffer(const char *fmt, ...) {
 }
 
 void *caminhao(void *arg) {
-    int caixas_total = 0;  // Contador total de caixas entregues
-    int id_caminhao = 1;   // Identificador incremental para cada caminhão
+    int id = *((int *)arg);
+    free(arg);
 
-    while (caixas_total < N) {
-        // Simula tempo aleatório entre a chegada de caminhões (em microsegundos)
-        int tempo_chegada = (rand() % T + 1) * 100000;
-        usleep(tempo_chegada);
+    // Determinar número de caixas para este caminhão
+    pthread_mutex_lock(&mutex);
+    if (n_caix_entregues >= N) {
+        pthread_mutex_unlock(&mutex);
+        log_buffer("Caminhão %d não entregará caixas, limite atingido\n", id);
+        return NULL;
+    }
+    int caixas_pedido = (rand() % C) + 1;
+    if (n_caix_entregues + caixas_pedido > N) {
+        caixas_pedido = N - n_caix_entregues;
+    }
+    pthread_mutex_unlock(&mutex);
 
-        // Gera quantidade aleatória de caixas no pedido (entre 1 e C)
-        int pedido = rand() % C + 1;
+    log_buffer("Caminhão %d, vai entregar %d caixa(s) na área de carga\n", id, caixas_pedido);
 
-        // Garante que o total de caixas não ultrapasse N
-        if (caixas_total + pedido > N) {
-            pedido = N - caixas_total;
-        }
-
-        log_buffer("Caminhão %d, vai entregar %d caixa(s) na área de carga\n", id_caminhao, pedido);
-
-        // Tenta descarregar todas as caixas do pedido de uma vez
+    int caixas_restantes = caixas_pedido;
+    while (caixas_restantes > 0) {
         pthread_mutex_lock(&mutex);
-        while (n_caix_areacarga + pedido > B) {
-            // Se não houver espaço suficiente, espera
+        // Verificar espaço na área de carga
+        while (n_caix_areacarga >= B && !finalizado) {
             pthread_cond_wait(&cond_caminhao, &mutex);
         }
+        if (finalizado) {
+            pthread_mutex_unlock(&mutex);
+            break;
+        }
+        // Descarregar o máximo possível de caixas
+        int caixas_descarga = B - n_caix_areacarga;
+        if (caixas_descarga > caixas_restantes) {
+            caixas_descarga = caixas_restantes;
+        }
+        n_caix_areacarga += caixas_descarga;
+        n_caix_entregues += caixas_descarga;
+        caixas_restantes -= caixas_descarga;
 
-        // Descarrega todas as caixas do pedido
-        n_caix_areacarga += pedido;
-        n_caix_entregues += pedido;
-        caixas_total += pedido;
+        log_buffer("Caminhão %d, descarregou %d caixa(s) na área de carga\n", id, caixas_descarga);
 
-        log_buffer("Caminhão %d, descarregou %d caixa(s) na área de carga\n", id_caminhao, pedido);
-
-        // Acorda funcionários que estejam esperando
+        // Sinalizar funcionários
         pthread_cond_broadcast(&cond_funcionario);
         pthread_mutex_unlock(&mutex);
 
-        id_caminhao++; // Próximo caminhão
+        // Simular tempo de espera entre descarregamentos
+        if (caixas_restantes > 0) {
+            usleep(100000); // 100ms
+        }
     }
 
-    // Marca que não haverá mais caminhões
-    pthread_mutex_lock(&mutex);
-    finalizado = true;
-    pthread_cond_broadcast(&cond_funcionario); // Acorda todos os funcionários
-    pthread_mutex_unlock(&mutex);
-
-    // Aguarda todas as caixas serem movidas para o estoque
-    pthread_mutex_lock(&mutex);
-    while (n_caix_estoque < N) {
-        pthread_cond_wait(&cond_caminhao, &mutex);
-    }
-    pthread_mutex_unlock(&mutex);
-
-    log_buffer("Encerrou thread caminhão\n");
+    log_buffer("Caminhão %d concluiu a entrega\n", id);
     return NULL;
 }
 
@@ -101,30 +99,22 @@ void *funcionario(void *arg) {
 
     while (true) {
         pthread_mutex_lock(&mutex);
-
-        // Aguarda até que haja caixas ou a simulação tenha finalizado
         while (n_caix_areacarga == 0 && !finalizado) {
             pthread_cond_wait(&cond_funcionario, &mutex);
         }
-
-        // Se todas as caixas foram entregues e movidas, encerra
         if (finalizado && n_caix_areacarga == 0) {
             pthread_mutex_unlock(&mutex);
             break;
         }
-
         if (n_caix_areacarga > 0) {
             n_caix_areacarga--;
             n_caix_estoque++;
             log_buffer("Funcionário %s, levou uma caixa ao estoque\n", nome);
-
-            // Acorda caminhão que esteja esperando espaço
             pthread_cond_signal(&cond_caminhao);
         }
-
         pthread_mutex_unlock(&mutex);
 
-        // Simula o tempo de transporte (1 a L décimos de segundo)
+        // Simular tempo de transporte
         int tempo = (rand() % L + 1) * 100000;
         usleep(tempo);
     }
@@ -164,31 +154,59 @@ int main(int argc, char *argv[]) {
     log_buffer("Iniciando simulação com N=%d, B=%d, C=%d, F=%d, T=%d, L=%d\n", N, B, C, F, T, L);
 
     pthread_t threads_funcionarios[F];
-    pthread_t thread_caminhao;
-
-    // Cria thread do caminhão
     log_buffer("Criada thread que simula chegada de caminhões\n");
-    pthread_create(&thread_caminhao, NULL, caminhao, NULL);
 
-    // Cria threads dos funcionários
+    // Criar threads dos funcionários
     for (int i = 0; i < F; i++) {
         int *id = malloc(sizeof(int));
         *id = i;
         pthread_create(&threads_funcionarios[i], NULL, funcionario, id);
     }
 
-    // Espera todas as threads de funcionários terminarem
+    // Criar threads de caminhões
+    int id_caminhao = 1;
+    pthread_t *threads_caminhoes = malloc(N * sizeof(pthread_t)); // Máximo N caminhões
+    int num_caminhoes = 0;
+
+    while (n_caix_entregues < N && criar_caminhoes) {
+        pthread_mutex_lock(&mutex);
+        if (n_caix_entregues >= N) {
+            criar_caminhoes = false;
+            pthread_mutex_unlock(&mutex);
+            break;
+        }
+        pthread_mutex_unlock(&mutex);
+
+        int *id = malloc(sizeof(int));
+        *id = id_caminhao++;
+        pthread_create(&threads_caminhoes[num_caminhoes++], NULL, caminhao, id);
+
+        // Simular intervalo de chegada
+        int tempo_chegada = (rand() % T + 1) * 1000000; // T em microssegundos
+        usleep(tempo_chegada);
+    }
+
+    // Sinalizar finalização
+    pthread_mutex_lock(&mutex);
+    criar_caminhoes = false;
+    finalizado = true;
+    pthread_cond_broadcast(&cond_funcionario);
+    pthread_mutex_unlock(&mutex);
+
+    // Esperar funcionários terminarem
     for (int i = 0; i < F; i++) {
         pthread_join(threads_funcionarios[i], NULL);
     }
 
-    // Espera a thread do caminhão terminar
-    pthread_join(thread_caminhao, NULL);
+    // Esperar caminhões terminarem
+    for (int i = 0; i < num_caminhoes; i++) {
+        pthread_join(threads_caminhoes[i], NULL);
+    }
+    free(threads_caminhoes);
 
+    log_buffer("Encerrou thread caminhão\n");
     log_buffer("Simulação concluída. Caixas no estoque: %d\n", n_caix_estoque);
 
-    // Imprime todo o conteúdo armazenado no buffer de saída
     printf("%s", output_buffer);
-
     return 0;
 }
